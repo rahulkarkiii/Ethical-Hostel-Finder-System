e5b6c38199f1e2203aa620002b93bb3561dd8cc2
rahulkarkiii <rahulkarkeee@gmail.com>
2026-05-19 13:02:49 +0545

    Done

--- src/main/java/com/hostell/hostel_finder/util/ValidationUtil.java
+++ src/main/java/com/hostell/hostel_finder/util/ValidationUtil.java
@@ -8,4 +8,29 @@ public class ValidationUtil {
     public static boolean isPositive(double value) {
         return value > 0;
     }
+
+    public static boolean isStrongPassword(String value) {
+        if (value == null || value.length() < 8) {
+            return false;
+        }
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasDigit = false;
+        boolean hasSymbol = false;
+
+        for (int i = 0; i < value.length(); i++) {
+            char ch = value.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                hasUpper = true;
+            } else if (Character.isLowerCase(ch)) {
+                hasLower = true;
+            } else if (Character.isDigit(ch)) {
+                hasDigit = true;
+            } else if (!Character.isWhitespace(ch)) {
+                hasSymbol = true;
+            }
+        }
+
+        return hasUpper && hasLower && hasDigit && hasSymbol;
+    }
 }
